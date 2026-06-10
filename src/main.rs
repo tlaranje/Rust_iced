c6713a741a119d73eb6205e20e9fc2eaddc29e12
tlaranje <cydogbi10@gmail.com>
2026-06-10 16:41:11 +0100

    Fix position of buttons

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use iced::{Element, Font};
-use iced::widget::{button, column, text};
+use iced::widget::{button, column, text, container};
 use iced::Length::Fill;
 use smart_default::SmartDefault;
+use iced::alignment::{Horizontal};
+use iced::Length;
 
 #[derive(SmartDefault)]
 struct Counter {
@@ -31,13 +33,20 @@ fn view(counter: &Counter) -> Element<'_, Message> {
             .width(Fill) 
             .center(),
 
-        button("+").on_press(Message::Increment),
+        button(
+            container(
+                text("Play").size(30)
+            )
+            .center_x(Length::Fill)
+            .center_y(Length::Fill)
+        ).width(150).height(50).on_press(Message::Increment),
         text(counter.value).size(24),
-        button("-").on_press(Message::Decrement),
+        button("Exit").on_press(Message::Decrement).width(150).height(50),
     ]
     .width(Fill)
     .height(Fill)
     .spacing(15)
+    .align_x(Horizontal::Center)
     .into()
 }
 
